e48416426431bbaecffa4e642b783b8ae476490a
Giacomo Cruz <giacomo1203@gmail.com>
2016-11-26 15:16:17 -0500

    Clase 09

---
+++ Clase 09/bootstrap/js/script.js
@@ -0,0 +1,21 @@
+//alert("hello");
+
+var nombre = "Eduardo Luis";
+var edad = 27;
+var sexo = true;
+var gustos = ["PS4", "Cantar", "Nadar", "Dormir"];
+
+var sueldo = 1200;
+var s_anual =   ;
+
+var msj = "Hola, mi nombre es " + nombre + " y mi edad es de " + edad;
+
+msj+= ". Y me gusta jugar " + gustos[0];
+msj += ". Yo naci el año " + (2016 - edad) ;
+
+/* + PROBANDO **/
+var cantidad = 5;
+cantidad = cantidad + 1;
+////////////////////////
+
+alert(msj);
