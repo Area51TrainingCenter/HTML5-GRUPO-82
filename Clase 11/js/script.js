a063c4d092d4da979c838bb2643c33689fc9d99d
Giacomo San <giacomo@MacBook-Pro-de-Giacomo.local>
2016-12-10 10:39:16 -0500

    Clase 11

--- Clase 11/js/script.js
+++ Clase 11/js/script.js
@@ -1,2 +1,11 @@
-$("#lista1 li").addClass('done');
-$("#lista1 li").removeClass('done');
+//$("#lista1 li").addClass('done');
+//$("#lista1 li").removeClass('done');
+
+function done(e){
+
+	if($(e).hasClass('done')){
+		$(e).removeClass('done');
+	}else{
+		$(e).addClass('done');
+	}
+}
